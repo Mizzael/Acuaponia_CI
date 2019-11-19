6532f169060e8c8509ad326a355f205fd7b6dbde
Missael <jonathanmissael_jimenez@ucol.mx>
2019-11-19 01:34:22 -0600

    Muestra Graficas de prueba aún nada de la tabla

--- application/views/Estado/Status.php
+++ application/views/Estado/Status.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/EstilosAcua.css">
     <link rel="shortcut icon" href="<?php echo base_url(); ?>/assets/image/logo.ico">
-    <script src="assets/js/jquery.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title> <?php  echo $titulo ?> | Gaia-System</title>
 </head>
@@ -15,7 +16,7 @@
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark mb-4">
     <a href="" class="navbar-brand">
-        <?php echo($this->session->userdata('user')['nombre']); ?> 
+      <?php echo($this->session->userdata('user')['nombre']); ?> 
     </a>
     <div class="collapse navbar-collapse mr-auto" id="navbarCollapse">
       <ul class="navbar-nav mr-auto">
@@ -26,17 +27,122 @@
       </form>
       <div>
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>Usuario/Logout">Salir</a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url(); ?>Usuario/Logout">Salir</a>
+          </li>
         </ul>
     </div>
     </div>
   </nav>
 </header>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div  class="my-md-8">
+  <canvas id="myChart" width="400" height="100"></canvas>
+  <script>
+
+    var parammeses=["Hola","Enero"];
+    var ctx = $('#myChart');
+    var chart = new Chart(ctx, {
+    type: 'line',
+    data: 
+    {
+      labels: ['January', 'February', 'March', 'April','May'],
+      datasets:
+      [
+        {
+          label: "Edades",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 5,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [10,20,30,40,50,60,70,80],
+          spanGaps: false,
+        },
+        {
+          label: "TemA",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 5,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [60,80,40,40,50,60,70,80],
+          spanGaps: false,
+        }
+      ]
+      // labels: ['January', 'February', 'March', 'April','May'],
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: 50,
+                    suggestedMax: 100
+                }
+            }]
+        }
+    }
+});
+  </script>
+</div>
+</body>
+</html>
     
    
     
 
-
-
+<!-- 
+{
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        } -->
