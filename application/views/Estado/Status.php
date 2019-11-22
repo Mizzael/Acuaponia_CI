5857d2a51f384c37136141582b7d2cd6a1c8c684
Missael <jonathanmissael_jimenez@ucol.mx>
2019-11-22 00:25:11 -0600

    Agregando Barra de prueba, parece

--- application/views/Estado/Status.php
+++ application/views/Estado/Status.php
@@ -35,14 +35,17 @@
       <div class="main_content">
         <div class="header text-center">Es bueno tenerte de vuelta!</div>  
           <div class="info">  
-            <div id="Grafica">
+            <!-- <div id="Grafica">
               <canvas id="myChart" width="400" height="100"></canvas>
-            </div>
+            </div> -->
+            <div id="canvas-container">
+		          <canvas id="chart" width="400" height="100"></canvas>
+	          </div>
         </div>
       </div>
   </div>
 </body>
-
+<!-- 
 <script>
   var Fecha=[];
   var TemR=[];
@@ -201,4 +204,73 @@
       });
     });
     
+</script> -->
+
+<script>
+  $(document).ready(function(){
+		
+		var datos = {
+			labels : ["Enero", "Febrero", "Marzo", "Abril", "Mayo"],
+			datasets : [{
+				label : "datos 1",
+				backgroundColor : "rgba(220,220,220,0.5)",
+				data : [4, 12, 9, 7, 5]
+			},
+			{
+
+				label : "datos 2",
+				backgroundColor : "rgba(151,187,205,0.5)",
+				data : [10,7,-5,6,5]
+			},
+			{
+				label : "datos 3",
+				backgroundColor : "rgba(151,100,205,0.5)",
+				data : [9,6,15,6,17]
+			}
+			]
+		};
+
+
+		var canvas = document.getElementById('chart').getContext('2d');
+		window.bar = new Chart(canvas, {
+			type : "bar",
+			data : datos,
+			options : {
+				elements : {
+					rectangle : {
+						borderWidth : 1,
+						borderColor : "rgb(0,255,0)",
+						borderSkipped : 'bottom'
+					}
+				},
+				responsive : true,
+				title : {
+					display : true,
+					text : "Estado del Sistema"
+				}
+			}
+		});
+
+		setInterval(function(){
+			var newData = [
+				[getRandom(),getRandom(),getRandom(),getRandom(),getRandom()],
+				[getRandom(),getRandom(),getRandom(),getRandom(),getRandom()],
+				[getRandom(),getRandom(),getRandom(),getRandom(),getRandom()],				
+			];
+
+			$.each(datos.datasets, function(i, dataset){
+				dataset.data = newData[i];
+			});
+			window.bar.update();
+		}, 3000);
+
+		
+
+
+		function getRandom(){
+			return Math.round(Math.random() * 100);
+		}
+
+
+	});
 </script>
